3f595c6e3bb802f53dde15eabe270ca1e58f2100
IlyaKhokhanov <hohanov.ilya@gmail.com>
2024-06-18 13:56:06 +0300

    done the project setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import htmlToElement from './utils/htmlToElement';
+import './style.css';
+
+document.body.innerHTML = 'hello';
+htmlToElement('hello');
---
+++ src/utils/htmlToElement.js
@@ -0,0 +1,3 @@
+export default function htmlToElement(htmlString) {
+  console.log(htmlString);
+}
