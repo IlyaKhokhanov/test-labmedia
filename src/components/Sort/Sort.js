aca79740522ec85af054c7134717f1eaa9d7b8c7
IlyaKhokhanov <hohanov.ilya@gmail.com>
2024-06-20 23:32:20 +0300

    add sort function

--- src/components/Sort/Sort.js
+++ src/components/Sort/Sort.js
@@ -1,14 +1,34 @@
 import { addElement } from '../../utils/utilits';
 import './Sort.css';
 
-export default function Sort() {
+export default function Sort(sort, sortList) {
   const sortElem = addElement('div', 'sort');
 
   const sortText = addElement('div', 'sort-text', 'Сортировка:');
 
   const sortRegistration = addElement('div', 'sort-item', 'Дата регистрации');
+  if (sort === 'ascDate' || sort === 'descDate') {
+    sortRegistration.classList.add('sort-item--active');
+  }
+  sortRegistration.addEventListener('click', () => {
+    if (sort === 'ascDate') {
+      sortList('descDate');
+    } else {
+      sortList('ascDate');
+    }
+  });
 
   const sortRating = addElement('div', 'sort-item', 'Рейтинг');
+  if (sort === 'ascRating' || sort === 'descRating') {
+    sortRating.classList.add('sort-item--active');
+  }
+  sortRating.addEventListener('click', () => {
+    if (sort === 'ascRating') {
+      sortList('descRating');
+    } else {
+      sortList('ascRating');
+    }
+  });
 
   sortElem.append(sortText, sortRegistration, sortRating);
   return sortElem;
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = document.querySelector('.main');
 const state = {
   data: null,
   currentData: null,
-  sort: null, // ascRating descRating ascData descData
+  sort: null,
   search: null,
   page: 1,
 };
@@ -19,15 +19,15 @@ function renderPage() {
   root.innerHTML = '';
   root.append(
     Search(state.search, state.sort, clearFilter),
-    Sort(),
+    Sort(state.sort, sortList),
     List(state.currentData, state.page, deleteItem),
     Pagination(state.page, state.currentData.length, changePage)
   );
 }
 
 fetchData().then((data) => {
-  state.currentData = data;
-  state.data = data;
+  state.data = data.slice();
+  state.currentData = data.slice();
   renderPage();
 });
 
@@ -37,7 +37,7 @@ function changePage(page) {
 }
 
 function clearFilter() {
-  state.currentData = state.data;
+  state.currentData = state.data.slice(0);
   state.sort = null;
   state.search = null;
   state.page = 1;
@@ -48,5 +48,37 @@ function deleteItem(id) {
   state.data = state.data.filter((el) => el.id !== id);
   state.currentData = state.currentData.filter((el) => el.id !== id);
   if (state.page - 1 === state.currentData.length / 5) state.page -= 1;
+  renderPage();
+}
+
+function sortList(sort) {
+  state.sort = sort;
+  state.page = 1;
+  switch (sort) {
+    case 'ascDate':
+      state.currentData = state.currentData.sort(
+        (a, b) => new Date(b.registration_date) - new Date(a.registration_date)
+      );
+      break;
+
+    case 'descDate':
+      state.currentData = state.currentData.sort(
+        (a, b) => new Date(a.registration_date) - new Date(b.registration_date)
+      );
+      break;
+
+    case 'ascRating':
+      state.currentData = state.currentData.sort((a, b) => b.rating - a.rating);
+      break;
+
+    case 'descRating':
+      state.currentData = state.currentData.sort((a, b) => a.rating - b.rating);
+      break;
+
+    default:
+      state.currentData = state.data;
+      break;
+  }
+
   renderPage();
 }
